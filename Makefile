79cbcfea59559ceb02dd9a6daa5215d31d4ef26d
gp201 <praneethg2001@gmail.com>
2025-05-03 21:44:49 -0700

    Add MD5 checksum comparison script
    - Introduced a new script `md5.sh` to generate and compare MD5 checksums for files in specified directories.
    - The script uses OpenSSL to compute checksums and checks for mismatches between expected and observed files.
    - Provides clear output for checksum matches and mismatches, as well as error messages for missing files.

---
+++ Makefile
@@ -0,0 +1,22 @@
+# Description: Makefile for Nextflow pipeline development
+
+.PHONY: clean md5sum test run-test
+
+clean:
+	rm -rf work/ testing/output .nextflow.log* .nextflow/
+
+md5sum:
+	bash assets/test/md5.sh
+
+run-test-nwk:
+	nextflow run main.nf -profile test,mamba -params-file assets/test/input/params_nwk.json
+
+run-test-nexus:
+	nextflow run main.nf -profile test,mamba -params-file assets/test/input/params_nexus.json
+
+test: clean
+	$(MAKE) run-test-nwk
+	$(MAKE) md5sum
+	rm -rf results/
+	$(MAKE) run-test-nexus
+	$(MAKE) md5sum
---
+++ assets/test/md5.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Function to generate MD5 checksum for a given file using openssl
+generate_md5_checksum() {
+    openssl md5 -binary "$1" | openssl base64
+}
+
+# Function to compare checksums of files in expected and observed directories
+compare_checksums() {
+    expected_dir="$1"
+    observed_dir="$2"
+
+    while IFS= read -r -d '' file_name; do
+        expected_file_path="$file_name"
+        # remove the expected_dir prefix from the file path
+        file_name="${file_name#$expected_dir/}"
+        observed_file_path="$observed_dir/$file_name"
+
+        if [ -f "$expected_file_path" ] && [ -f "$observed_file_path" ]; then
+            expected_checksum=$(generate_md5_checksum "$expected_file_path")
+            observed_checksum=$(generate_md5_checksum "$observed_file_path")
+
+            if [ "$expected_checksum" != "$observed_checksum" ]; then
+                echo -e "\033[0;31mChecksum mismatch for file: $file_name\033[0m"
+                echo "Expected: $expected_checksum"
+                echo "Observed: $observed_checksum"
+                exit 1
+            else
+                echo "Checksum matched for file: $file_name"
+            fi
+        else
+            echo "File not found: $file_name at $expected_file_path or $observed_file_path"
+            exit 1
+        fi
+    done < <(find "$expected_dir" -type f -print0)
+}
+
+compare_checksums "assets/test/results" "results"
